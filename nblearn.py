d62497d37b681e971d522e73f8a3e16fa7162865
Francesca Nannizzi <fnannizzi@gmail.com>
2014-01-28 23:22:27 -0800

    Able to compute word frequencies.

--- nblearn.py
+++ nblearn.py
@@ -3,19 +3,26 @@
 import sys
 import re
 
-class Feature:
-    def __init__(self, val):
-        self.value = val
-        self.frequency = 0
+# class to store information used in the model
+class Word:
+    def __init__(self, t):
+        self.text = t
+        self.frequency = 1
         self.probability = 0
 
-def match_string(word, value):
+    def incr(self):
+        self.frequency += 1
+
+
+# function to match strings exactly
+def match_string(word, text):
     boundary = "\\b"
-    pattern = re.compile(boundary + value + boundary)
-    if pattern.match(word):
-        return true
+    pattern = re.compile(boundary + re.escape(text) + boundary)
+    if pattern.match(re.escape(word)):
+        return True
     else:
-        return false
+        return False
+
 
 def train():
 
@@ -29,7 +36,7 @@ def train():
 
 
     classes = [] # list of possible classes
-    class_features = [] # list of lists, separate list of features for each class
+    class_vocabulary = [] # list of lists, separate list of vocabulary words for each class
 
     with open(training_filename) as f:
         for line in f:
@@ -44,9 +51,31 @@ def train():
             # get the index of the class so we add the features to the correct index
             class_index = classes.index(classname)
             
+            # if the class vocabulary has not been initialized, create a new vocabulary list
+            if len(class_vocabulary) <= class_index:
+                temp_array = []
+                class_vocabulary.append(temp_array)
+                first_word_and_text = text.split(' ', 1)
+                first_word = first_word_and_text[0]
+                text = first_word_and_text[1]
+                new_word = Word(first_word.lower())
+                class_vocabulary[class_index].append(new_word)
+                print class_vocabulary[class_index]
+
             for word in text.split(' '):
-                matches = filter(lambda x: match_string(word, x.value), class_features) # store all matching words in matches
-                print matchesgit
+                word = word.lower()
+                matches = filter(lambda x: match_string(word, x.text), class_vocabulary[class_index]) # store all matching words in matches
+                    
+                # if the word is not already in the class' vocabulary, add it
+                if not matches:
+                    new_word = Word(word)
+                    class_vocabulary[class_index].append(new_word)
+                elif word not in matches:
+                    new_word = Word(word)
+                    class_vocabulary[class_index].append(new_word)
+                else:
+                    class_vocabulary[class_index].incr()
+
 
 
 
